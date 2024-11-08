8dc1f9b90c1e56b93304b958e5b92ef3d2675ec3
Joenel Jamero Jr. <144349912+joenel-jamero-jr@users.noreply.github.com>
2024-11-08 13:25:12 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,26 +3,35 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Ubunto Server PHP Deployment</title>
+	<title>Ubuntu-Server-PHP-Deployment</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
-	<p>My first Ubunto Server PHP Deployment</p>
+	<p style="font-weight: bold;">My First Ubunto Server PHP Deployment Joenel Jamero Jr.</p>
 
 </body>
 
- <div class="container">
+<div class="container">
 <label>Firstname</label>
 <input type="text" name="firstname" class="form-control">
-  </div>
-   <div class="container">
-<label>Middle</label>
-<input type="text" name="firstname" class="form-control">
-  </div> <div class="container">
+</div>
+
+<div class="container">
+<label>Middlename</label>
+<input type="text" name="middlename" class="form-control">
+</div> 
+
+<div class="container">
 <label>Lastname</label>
-<input type="text" name="firstname" class="form-control">
-  </div>
+<input type="text" name="lastname" class="form-control">
+</div>
+
+<div class="container">
+<label>Age</label>
+<input type="text" name="age" class="form-control">
+</div>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
