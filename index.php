98520b87e690addc47f4dd0913904388232c679c
Joenel Jamero Jr. <144349912+joenel-jamero-jr@users.noreply.github.com>
2024-11-08 20:24:25 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Ubuntu-Server-PHP-Deployment</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubuntu-Server-PHP-Deployment</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<p style="font-weight: bold;">My First Ubunto Server PHP Deployment Joenel Jamero Jr.</p>
+    <div class="form-container">
+        <h1>Registration Form</h1>
+        <form action="process_registration.php" method="post">
+            <label for="firstname">First Name:</label>
+            <input type="text" id="firstname" name="firstname" required>
 
+            <label for="middlename">Middle Name:</label>
+            <input type="text" id="middlename" name="middlename">
 
-	<p style="font-weight: bold;">My First Ubunto Server PHP Deployment Joenel Jamero Jr.</p>
+            <label for="lastname">Last Name:</label>
+            <input type="text" id="lastname" name="lastname" required>
 
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" min="1" required>
+
+            <label for="birthdate">Birthdate:</label>
+            <input type="date" id="birthdate" name="birthdate" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Register</button>
+        </form>
+    </div>
 </body>
-
-<div class="container">
-<label>Firstname</label>
-<input type="text" name="firstname" class="form-control">
-</div>
-
-<div class="container">
-<label>Middlename</label>
-<input type="text" name="middlename" class="form-control">
-</div> 
-
-<div class="container">
-<label>Lastname</label>
-<input type="text" name="lastname" class="form-control">
-</div>
-
-<div class="container">
-<label>Age</label>
-<input type="text" name="age" class="form-control">
-</div>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
